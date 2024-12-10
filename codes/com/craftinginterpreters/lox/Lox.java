c08ed4e22fd2cd365d750eb77403c8c384399217
ran71093 <cire21093@g.nihon-u.ac.jp>
2024-12-10 11:50:49 +0900

    isAlphaNumeric

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-//import java.util.List;
+import java.util.List;
 //import java.util.Scanner;
 
 public class Lox {
@@ -43,13 +43,13 @@ public class Lox {
   }
 
   private static void run(String source) {
-    /*Scanner scanner = new Scanner(source);
+    Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
 
     // For now, just print the tokens.
     for (Token token : tokens) {
       System.out.println(token);
-    }*/
+    }
    System.out.println(source); 
   }
   static void error(int line, String message) {
