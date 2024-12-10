1994712b2017d04e95f7c2170e6196a02ac43779
ran71093 <cire21093@g.nihon-u.ac.jp>
2024-12-10 11:30:24 +0900

    scanner new

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -6,8 +6,8 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Scanner;
+//import java.util.List;
+//import java.util.Scanner;
 
 public class Lox {
   static boolean hadError = false;
@@ -43,13 +43,14 @@ public class Lox {
   }
 
   private static void run(String source) {
-    Scanner scanner = new Scanner(source);
+    /*Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
 
     // For now, just print the tokens.
     for (Token token : tokens) {
       System.out.println(token);
-    }
+    }*/
+   System.out.println(source); 
   }
   static void error(int line, String message) {
     report(line, "", message);
